609736efac8a3292e3c67df528438f474dcfeedf
Eric Abell <ericabell@users.noreply.github.com>
2017-08-08 08:42:04 -0500

    Assignment complete

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        { // put 1 & 2 in a code block to get rid of compile error
+            int count = 0;
+            System.out.println(count);
+        }
+
+        // attempt to declare the same variable
+        // gives a compile error
+        // after wrapping above in code block, compile error
+        // vanishes
+        int count;
+
+
     }
 }
